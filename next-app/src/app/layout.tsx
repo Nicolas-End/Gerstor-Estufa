047e25942619222493265afa4d0430ae13458b65
Narciso <nicolasvalsi203@gmail.com>
2025-02-26 08:10:38 -0300

    [fix] Head no layout

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>
+          Seja Bem vindo ao Today
+        </title>
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
